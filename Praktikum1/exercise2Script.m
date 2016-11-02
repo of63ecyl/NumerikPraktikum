2227472bf390778862fc4806e15bec3216148ec8
Ullrich <of63ecyl@cip-54-47.mi.uni-erlangen.de>
2016-11-02 14:09:01 +0100

    ex2

--- Praktikum1/exercise2Script.m
+++ Praktikum1/exercise2Script.m
@@ -5,14 +5,24 @@ h3 = 0.05;
 
 lim = 1;
 
-t1 = [0:h1:lim];
-t2 = [0:h2:lim];
-t3 = [0:h3:lim];
+t = linspace(0,1);
+t1 = 0:h1:lim;
+t2 = 0:h2:lim;
+t3 = 0:h3:lim;
 
 y0 = 1;
 
 inputFun = @(t,c) 1./(t + c);
 
+y =  1./(t + 1);
 y1 = expl_euler(inputFun, t1, y0);
 y2 = expl_euler(inputFun, t2, y0);
 y3 = expl_euler(inputFun, t3, y0);
+
+figure(1)
+plot(t1,y1,'*-')
+hold on
+plot(t2,y2,'*-')
+plot(t3,y3,'*-')
+plot(t,y)
+hold off
--- Praktikum1/expl_euler.m
+++ Praktikum1/expl_euler.m
@@ -1,15 +1,16 @@
 % exercise 2
 function y = expl_euler(inputFun, t, y0)
 
-y = zeros(1,length(t));
+y = zeros(1,length(t)-1);
 y(1) = y0;
 
 h = diff(t(1:2));
 
+tmp = zeros(1,length(t)-1);
 
-for n = 1:length(t)
-    tmp = feval(inputFun,t(n),1/y0);
-    y(n+1) = y(n) + h * tmp;
+for n = 1:length(t)-1
+    tmp(n) = feval(inputFun,t(n),1/y0);
+    y(n+1) = y(n) + h * tmp(n);
 end
   
 end
